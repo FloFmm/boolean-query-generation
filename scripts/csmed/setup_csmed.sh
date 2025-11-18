9cf197cf0733a07f5d18746a959d864d91f31429
florian <florian_maurus.mueller@mailbox.tu-dresden.de>
2025-11-18 11:08:01 +0100

    changed setup_csmed

--- scripts/csmed/setup_csmed.sh
+++ scripts/csmed/setup_csmed.sh
@@ -1,9 +1,31 @@
-# clone code repos (my forks)
-echo "=== Cloning code repositories ==="
-[ ! -d "systematic-review-datasets" ] && git clone git@github.com:FloFmm/systematic-review-datasets.git
-[ ! -d "CSMeD-baselines" ] && git clone git@github.com:FloFmm/CSMeD-baselines.git
+# Function to clone or update a repo
+update_repo() {
+    local repo_url=$1
+    local folder_name=$2
+    if [ -d "$folder_name" ]; then
+        echo "=== Updating $folder_name ==="
+        cd "$folder_name"
+        git pull
+        cd ..
+    else
+        echo "=== Cloning $folder_name ==="
+        git clone "$repo_url" "$folder_name"
+    fi
+}
 
+# clone/update code repos
+update_repo git@github.com:FloFmm/systematic-review-datasets.git systematic-review-datasets
+update_repo git@github.com:FloFmm/CSMeD-baselines.git CSMeD-baselines
+
+# clone/update data repos
+update_repo https://github.com/ielab/SIGIR2017-SysRev-Collection.git SIGIR2017-SysRev-Collection
+update_repo https://github.com/CLEF-TAR/tar.git tar
+update_repo https://github.com/Amal-Alharbi/Systematic_Reviews_Update.git Systematic_Reviews_Update
+
+# conda env set up
 echo "=== Setting up environment ==="
+conda init bash
+source ~/.bashrc
 cd systematic-review-datasets
 if [ ! -d "./csmed" ]; then
     conda create --prefix ./csmed python=3.10
@@ -11,12 +33,6 @@ fi
 conda activate csmed
 pip install -r requirements.txt
 pip install -r experiment_requirements.txt
-
-# clone data repos
-echo "=== Cloning data repositories ==="
-[ ! -d "SIGIR2017-SysRev-Collection" ] && git clone https://github.com/ielab/SIGIR2017-SysRev-Collection.git
-[ ! -d "tar" ] && git clone https://github.com/CLEF-TAR/tar.git
-[ ! -d "Systematic_Reviews_Update" ] && git clone https://github.com/Amal-Alharbi/Systematic_Reviews_Update.git
 
 # get review details from cochrane
 echo "=== Converting datasets ==="
