140963bd0949cd8105986c0ee11e702d266fc933
florian <florian_maurus.mueller@mailbox.tu-dresden.de>
2026-01-30 16:25:59 +0100

    backup.sh

---
+++ scripts/backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DEST=/home/florian/Data/dev/boolean-query-generation/data
+mkdir -p "$DEST/statistics"
+mkdir -p "$DEST/statistics/optuna"
+SOURCE=dataport1.hpc.tu-dresden.de:/data/horse/ws/flml293c-master-thesis/boolean-query-generation/data/
+rsync -av --progress "$SOURCE/reports" "$DEST"
+rsync -av --progress "$SOURCE/statistics/final" "$DEST/statistics"
+rsync -av --progress "$SOURCE/statistics/images" "$DEST/statistics"
+rsync -av --progress "$SOURCE/statistics/optuna/images" "$DEST/statistics/optuna"
+rsync -av --progress --exclude='*.pkl' --exclude='*.privatelock' --exclude='*.lock' $SOURCE/statistics/optuna/best* "$DEST/statistics/optuna"
+
+DEST=/home/florian/Data/dev/systematic-review-datasets/data
+mkdir -p "$DEST"
+mkdir -p "$DEST/rankings"
+SOURCE=dataport1.hpc.tu-dresden.de:/data/horse/ws/flml293c-master-thesis/systematic-review-datasets/data
+rsync -av --progress "$SOURCE/bag_of_words" "$DEST"
+rsync -av --progress "$SOURCE/external" "$DEST"
+rsync -av --progress "$SOURCE/rankings/pubmedbert" "$DEST/rankings"
