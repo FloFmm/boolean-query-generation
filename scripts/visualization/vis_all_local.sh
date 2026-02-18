2cd98360478b0276bdcf6a09941ad3060e395c0d
florian <florian_maurus.mueller@mailbox.tu-dresden.de>
2026-02-18 10:24:58 +0100

    updated vis all script

--- scripts/visualization/vis_all_local.sh
+++ scripts/visualization/vis_all_local.sh
@@ -3,3 +3,11 @@ python -m app.visualization.precision_recall_heatmap
 python -m app.visualization.top_k_type_comparison
 python -m app.visualization.recall_curve_by_bucket
 python -m app.visualization.compare_retrievers
+
+# tables
+python -m app.visualization.feature_replacement_map.py
+python -m app.visualization.tables.query_example_table.py
+python -m app.visualization.tables.result_table.py
+python -m app.visualization.tables.handmade_table.py
+python -m app.visualization.tables.best_worst_table.py
+
