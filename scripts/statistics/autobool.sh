9dd81cbc5bcfd7547d9d55d9b33eff31990366a4
Florian Maurus Mueller <flml293c@n1719.barnard.hpc.tu-dresden.de>
2026-02-24 14:14:44 +0100

    Update from HPC

--- scripts/statistics/autobool.sh
+++ scripts/statistics/autobool.sh
@@ -9,34 +9,55 @@
 # Path where the new conda env should live
 set -e
 
-ENV_PATH=/data/horse/ws/flml293c-master-thesis/systematic-review-dataset/autobool_conda
-ENV_PY=/data/horse/ws/flml293c-master-thesis/systematic-review-dataset/autobool_conda/bin/python
-PYTHON_VERSION=3.10
-# Load conda (adjust if your cluster uses a different module name)
-source "$(conda info --base)/etc/profile.d/conda.sh"
-# Create env only if it doesn't exist
-if [ ! -f "$ENV_PATH/bin/python" ]; then
-echo "Creating conda environment at $ENV_PATH"
-conda create --prefix "$ENV_PATH" python=$PYTHON_VERSION -y
-conda activate "$ENV_PATH"
+# Load Anaconda / Python module
+ml release/23.04
+ml Anaconda3/2022.05
+ml GCC/11.3.0
 
-    # Install minimal dependencies
-    pip install --no-cache-dir \
-        "transformers>=4.40" \
-        "tokenizers>=0.15" \
-        accelerate \
-        huggingface_hub \
-        torch
-
-
-else
-echo "Using existing environment at $ENV_PATH"
-conda activate "$ENV_PATH"
+# conda env set up
+echo "=== Setting up environment ==="
+cd systematic-review-datasets
+if [ ! -d "./autobool_conda" ]; then
+    conda create --prefix ./autobool_conda python=3.10 -y
 fi
 
-# Go to project directory
+ENV_PATH=/data/horse/ws/flml293c-master-thesis/systematic-review-datasets/autobool_conda
+ENV_PY=/data/horse/ws/flml293c-master-thesis/systematic-review-datasets/autobool_conda/bin/python
+$ENV_PY -m pip install \
+    "transformers>=4.40" \
+    "tokenizers>=0.15" \
+    accelerate \
+    huggingface_hub \
+    torch
 
-cd boolean-query-generation
+# PYTHON_VERSION=3.10
+# # Load conda (adjust if your cluster uses a different module name)
+# source "$(conda info --base)/etc/profile.d/conda.sh"
+# # Create env only if it doesn't exist
+# if [ ! -f "$ENV_PATH/bin/python" ]; then
+# echo "Creating conda environment at $ENV_PATH"
+# conda create --prefix "$ENV_PATH" python=$PYTHON_VERSION -y
+# conda activate "$ENV_PATH"
 
-# Run script using env python
-$ENV_PY -m app.statistics.autobool
+#     # Install minimal dependencies
+#     $ENV_PY -m pip install --no-cache-dir --no-user \
+#         "transformers>=4.40" \
+#         "tokenizers>=0.15" \
+#         accelerate \
+#         huggingface_hub \
+#         torch
+
+
+# else
+# echo "Using existing environment at $ENV_PATH"
+# conda activate "$ENV_PATH"
+# fi
+
+# # Go to project directory
+
+# cd boolean-query-generation
+
+# # Run script using env python
+# $ENV_PY -m app.statistics.autobool
+
+
