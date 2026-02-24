975a1e4e11af7f7accf5d8937a914e0d0b865db0
Florian Maurus Mueller <flml293c@n1719.barnard.hpc.tu-dresden.de>
2026-02-24 09:06:34 +0100

    Update from HPC

--- scripts/statistics/autobool.sh
+++ scripts/statistics/autobool.sh
@@ -6,7 +6,37 @@
 #SBATCH --mail-type=end
 #SBATCH --mail-user=florian_maurus.mueller@mailbox.tu-dresden.de
 
-CSMED_PY=/data/horse/ws/flml293c-master-thesis/systematic-review-datasets/csmed_conda/bin/python
+# Path where the new conda env should live
+set -e
+
+ENV_PATH=/data/horse/ws/flml293c-master-thesis/systematic-review-dataset/autobool_conda
+ENV_PY=/data/horse/ws/flml293c-master-thesis/systematic-review-dataset/autobool_conda/bin/python
+PYTHON_VERSION=3.10
+# Load conda (adjust if your cluster uses a different module name)
+source "$(conda info --base)/etc/profile.d/conda.sh"
+# Create env only if it doesn't exist
+if [ ! -f "$ENV_PATH/bin/python" ]; then
+echo "Creating conda environment at $ENV_PATH"
+conda create --prefix "$ENV_PATH" python=$PYTHON_VERSION -y
+conda activate "$ENV_PATH"
+
+    # Install minimal dependencies
+    pip install --no-cache-dir \
+        "transformers>=4.40" \
+        "tokenizers>=0.15" \
+        accelerate \
+        huggingface_hub \
+        torch
+
+
+else
+echo "Using existing environment at $ENV_PATH"
+conda activate "$ENV_PATH"
+fi
+
+# Go to project directory
 
 cd boolean-query-generation
-$CSMED_PY -m app.statistics.autobool
+
+# Run script using env python
+$ENV_PY -m app.statistics.autobool
