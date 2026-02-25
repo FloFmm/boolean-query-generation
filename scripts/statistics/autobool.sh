bd5cad2202b9e83151eff1aa6a630179a0c9c2ce
florian <florian_maurus.mueller@mailbox.tu-dresden.de>
2026-02-25 14:00:48 +0100

    autobool script

--- scripts/statistics/autobool.sh
+++ scripts/statistics/autobool.sh
@@ -6,6 +6,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=end
 #SBATCH --mail-user=florian_maurus.mueller@mailbox.tu-dresden.de
+#SBATCH --mem=150000
 
 CSMED_PY=/data/horse/ws/flml293c-master-thesis/systematic-review-datasets/csmed_conda/bin/python
 
